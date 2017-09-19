465820055ece9d87c300d856d753ef478571bf47
ResponsoMundus <gamesluook_hi@126.com>
2017-09-19 12:06:46 +0800

    hahaha

--- code_lesson1/ex1/ClassifyNB.py
+++ code_lesson1/ex1/ClassifyNB.py
@@ -3,8 +3,7 @@ def classify(features_train, labels_train):
     ### create classifier
     ### fit the classifier on the training features and labels
     ### return the fit classifier
-    
-    
+
     ### your code goes here!
 	from sklearn.naive_bayes import GaussianNB
 	clf = GaussianNB()
